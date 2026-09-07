b93d309565d2c6ef9baf6a2de0b7cd502a9ce4ee
WonGoon <wongoon@gmail.com>
2026-09-08 08:48:57 +0900

    GitHub Pages 설정 제거, Vercel 배포로 전환

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/secondbrain',
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
